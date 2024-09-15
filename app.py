0ef2ee8a9eebbcab1d3f9c7cad585e936742ec47
webby <webberic92@yahoo.com>
2024-09-15 17:17:13 -0400

    it did deploy the VPC and RDS. deleted... todo test 2 nodes connecting and load to db.

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-# app.py or cdk_app.py
-from aws_cdk import core
+from aws_cdk import App
+from constructs import Construct
+from infrastructure.vpc_stack import MyVpcStack
 from database_and_storage.rds_stack import RdsStack
-from infrastructure.vpc_stack import VpcStack
 
-class MyCdkApp(core.App):
+class MyCdkApp(App):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
         # Create VPC stack
-        vpc_stack = VpcStack(self, "VpcStack")
+        vpc_stack = MyVpcStack(self, "VpcStack")
 
         # Create RDS stack
         RdsStack(self, "RdsStack", vpc=vpc_stack.vpc)
 
 app = MyCdkApp()
+app.synth()
