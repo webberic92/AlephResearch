535060454f1e12bbf0e2af0759d09c857560eb97
webby <webberic92@yahoo.com>
2025-02-16 20:16:08 -0500

    ready to create baseline 3 node X tx Y rounds

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ class TestAleph(Stack):
         super().__init__(scope, id, **kwargs)
 
         INSTANCES_NUMBER = 3 # Define the number of instances
-        NUMBER_OF_TRANSACTIONS = 2  # Define the number of transactions per batch
+        NUMBER_OF_TRANSACTIONS = 2  # Define the number of transactions per round per that node
         TRANSACTION_SIZE = 256 #Bytes how many bytes per transaction
-        SHARD_SIZE = 4
-        TOTAL_ROUNDS = 5
+        SHARD_SIZE = 4 # Number of data shards for erasure coding
+        TOTAL_ROUNDS = 5 
         unique_id = datetime.now().strftime("%Y%m%d%H%M")
 
         # Create a VPC within the scope of this Stack
