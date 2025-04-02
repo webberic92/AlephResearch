9bebd389078fe85d574dc87921ef1a7471f4956c
webby <webberic92@yahoo.com>
2025-04-01 22:06:55 -0400

    testing 5 25 25

--- src/handlers/handle_commit.rs
+++ src/handlers/handle_commit.rs
@@ -138,7 +138,7 @@ pub async fn handle_commit(
                 };
 
                 let s3_upload_cmd = format!(
-                    r#"(S3_FOLDER="logs/nodes_N{instances}_T{txs}_R{rounds}/node-{node_id}" && \
+                    r#"(S3_FOLDER="logs/ORIG_N{instances}_T{txs}_R{rounds}/node-{node_id}" && \
                     aws s3 cp /home/aleph-node/logs/ s3://aleph-research/$S3_FOLDER/ --recursive --quiet) &"#,
                 );
 
