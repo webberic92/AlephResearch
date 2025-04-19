c94087be135d4965cad86679f03bac9f1066f9d6
webby <webberic92@yahoo.com>
2025-04-19 19:28:08 -0400

    KIND OF WORKING: 10nodes 512txs per batch got to 46 out of 51 rounds.

--- src/utils/config_util.rs
+++ src/utils/config_util.rs
@@ -11,13 +11,13 @@ use crate::structs::toml_config::TomlConfig;
 
 /// Load configuration
 pub fn load_config(path: Option<&str>) -> TomlConfig {
-    let config_path = path.unwrap_or("/home/aleph-node/aleph-node-config.toml");
+    let config_path = path.unwrap_or("/aleph/aleph-node-config.toml");
     let config_contents = std::fs::read_to_string(config_path).expect("Failed to read configuration file.");
     toml::from_str(&config_contents).expect("Failed to parse configuration.")
 }
 
 pub fn save_config(toml_config: &TomlConfig, path: Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
-    let config_path = path.unwrap_or("/home/aleph-node/aleph-node-config.toml");
+    let config_path = path.unwrap_or("/aleph/aleph-node-config.toml");
     let config_contents = toml::to_string(&toml_config)
         .expect("Failed to serialize configuration.");
     std::fs::write(config_path, config_contents)
