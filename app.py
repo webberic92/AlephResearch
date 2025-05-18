6e388c1dd4698728726ebe8ae934494bb2ea70c0
webby <webberic92@yahoo.com>
2025-05-18 17:54:20 -0400

    WORKED 32nodes 1024tx per batch.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class TestAleph(Stack):
         super().__init__(scope, id, **kwargs)
        
         INSTANCE_TYPE ="c5n.xlarge" # Define the instance type
-        INSTANCES_NUMBER = 16 # Define the number of instances
+        INSTANCES_NUMBER = 32 # Define the number of instances
         BATCH_SIZE =  1024# Define the number of transactions in a batch
         TRANSACTION_SIZE = 256 #Bytes how many bytes per transaction
         SHARD_SIZE = max(1, min(BATCH_SIZE, INSTANCES_NUMBER - INSTANCES_NUMBER // 3))
