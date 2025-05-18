4e6c44122cbbc2ec5d0b437cec28b2ca79e51dbf
webby <webberic92@yahoo.com>
2025-05-18 17:21:54 -0400

    WORKED 16nodes 1024tx 3 rounds.

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ class TestAleph(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
        
-        INSTANCE_TYPE ="c5n.large" # Define the instance type
+        INSTANCE_TYPE ="c5n.xlarge" # Define the instance type
         INSTANCES_NUMBER = 16 # Define the number of instances
-        BATCH_SIZE =  256# Define the number of transactions in a batch
+        BATCH_SIZE =  1024# Define the number of transactions in a batch
         TRANSACTION_SIZE = 256 #Bytes how many bytes per transaction
         SHARD_SIZE = max(1, min(BATCH_SIZE, INSTANCES_NUMBER - INSTANCES_NUMBER // 3))
         # TOTAL_ROUNDS = max(1, BATCH_SIZE // INSTANCES_NUMBER)
