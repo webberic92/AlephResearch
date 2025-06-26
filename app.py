8540742c4f046a7ba6653393369a5c8c2b18303a
webby <webberic92@yahoo.com>
2025-06-25 20:25:43 -0700

    clean up

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ class TestAleph(Stack):
         BATCH_SIZE =  1028#(TX PER BATCH) Define the number of transactions in a batch
         TRANSACTION_SIZE = 256 #Bytes how many bytes per transaction
         SHARD_SIZE = max(1, min(BATCH_SIZE, INSTANCES_NUMBER - INSTANCES_NUMBER // 3))
-        # TOTAL_ROUNDS = max(1, BATCH_SIZE // INSTANCES_NUMBER)
         TOTAL_ROUNDS = 10 # Define the number of rounds
         unique_id = datetime.now().strftime("%Y%m%d%H%M")
 
